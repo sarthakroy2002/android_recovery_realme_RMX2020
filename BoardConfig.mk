3f28f0380b7b4149269998876bff329a81650ec7
Aryan Sinha <sinha.aryan03@gmail.com>
2021-01-15 09:34:43 +0000

    RMX2185: Fix fstab Path
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,3 +131,4 @@ TW_SCREEN_BLANK_ON_BOOT := true
 PLATFORM_VERSION := 16.1.0
 TW_OZIP_DECRYPT_KEY := "0000"
 TW_INCLUDE_LOGICAL := oppo_product oppo_engineering common_preload
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
