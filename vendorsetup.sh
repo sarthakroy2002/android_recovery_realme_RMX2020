da7df49be26adab2aa6430f9b161e85eecb025e4
Hemanth <hemanthjabalpuri@gmail.com>
2021-05-23 19:29:33 +0530

    Add OrangeFox specific build vars

---
+++ vendorsetup.sh
@@ -0,0 +1,74 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="RMX2185"
+
+fox_get_target_device() {
+  local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+  if [ -n "$chkdev" ]; then
+    FOX_BUILD_DEVICE="$FDEVICE"
+  else
+    chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+    [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+  fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+  export TW_DEFAULT_LANGUAGE="en"
+  export OF_KEEP_DM_VERITY=1
+  export OF_QUICK_BACKUP_LIST="/boot;/data;"
+  export OF_SCREEN_H=2400
+  export OF_HIDE_NOTCH=1
+  export OF_FLASHLIGHT_ENABLE=0
+  export OF_USE_MAGISKBOOT=1
+  export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+  export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+  export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+  export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+  export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+
+  # OZIP flashing support
+  export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+  export OF_SUPPORT_OZIP_DECRYPTION=1
+
+  export OF_MAINTAINER="HemanthJabalpuri"
+  export FOX_VERSION="R11.1_0"
+  export FOX_BUILD_TYPE="test"
+  export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+  export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+  export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+  export FOX_EXCLUDE_NANO_EDITOR=1
+  export FOX_REMOVE_BASH=1
+  export FOX_DELETE_AROMAFM=1
+  #export FOX_DELETE_MAGISK_ADDON=1
+  export FOX_USE_SPECIFIC_MAGISK_ZIP="$HOME/OrangeFox_10/Magisk.zip"
+  export FOX_DELETE_INITD_ADDON=1
+
+  # let's see what are our build VARs
+  if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+    export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+    export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+    export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+    export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+  fi
+fi
