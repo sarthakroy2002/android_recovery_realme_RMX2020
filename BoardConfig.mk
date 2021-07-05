fa67af078391ec472c2b64e5be136127410e179c
sarthakroy2002 <sarthakroy2002@gmail.com>
2021-07-05 11:56:16 +0530

    RMX2020: Fix loop errors

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ TARGET_USES_UEFI := true
 TARGET_BOARD_PLATFORM := mt6768
 
 # Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
