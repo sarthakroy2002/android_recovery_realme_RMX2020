da89a2c71105f8cd981ed669826e7e362a2e829d
SamarV-121 <samarvispute121@gmail.com>
2021-07-05 11:53:09 +0530

    RMX2020: Include repacktools and resetprop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,8 @@ TW_CUSTOM_CPU_TEMP_PATH := /sys/class/power_supply/battery/temp
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_NO_SCREEN_BLANK := true
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
