8f63812ac1331548b4562c4872bd18735ec8f93b
SamarV-121 <samarvispute121@pm.me>
2021-09-04 21:58:27 +0530

    RMX2020: Add custom temperature sensor path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,6 +149,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone3/temp
 TW_NO_SCREEN_BLANK := true
 TW_SCREEN_BLANK_ON_BOOT := true
 #TW_Y_OFFSET := 50
