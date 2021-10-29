ab3c72fed21a10e2f326597e2ee79d0774790207
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-29 07:05:38 +0000

    RMX2020: Major fixes and cleanup
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- twrp_RMX2020.mk
+++ twrp_RMX2020.mk
@@ -19,6 +19,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -35,3 +36,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.bootimage.build.date.utc=0 \
+    ro.build.date.utc=0
