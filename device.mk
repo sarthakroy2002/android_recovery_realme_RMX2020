e0f2aaa9035c24a480c5b49e04c2d5a2373a8c9e
Sarthak Roy <sarthakroy2002@gmail.com>
2022-06-20 18:58:45 +0530

    RMX2020: Fixes for A12.1
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I3f1178a36a8950e4f45a1a07fb73bc79421e1290

---
+++ device.mk
@@ -0,0 +1,33 @@
+
+#
+# Copyright (C) 2021 Team Win Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastboot packages
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+	libkeymaster4 \
+	libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
--- twrp_RMX2020.mk
+++ twrp_RMX2020.mk
@@ -16,7 +16,6 @@
 
 # Release name
 PRODUCT_RELEASE_NAME := RMX2020
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Extra VNDK Versions
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
@@ -27,10 +26,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RMX2020/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2020
@@ -43,9 +43,4 @@ TW_DEVICE_VERSION := By sarthakroy2002 (For RealmeUI 1.0)
 # HACK: Set vendor patch level
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0 \
-    ro.vendor.build.security_patch=2099-12-31
-
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+    ro.vendor.build.security_patch=2127-12-31
